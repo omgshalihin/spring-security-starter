4e939635260bc93192b1ec17f298f90b34e027c3
omgshalihin <shalihinsgp@gmail.com>
2023-03-29 11:28:39 +0200

    user repository created

--- src/main/java/com/springSecurity/starter/repository/UserRepository.java
+++ src/main/java/com/springSecurity/starter/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.springSecurity.starter.repository;
 
 import com.springSecurity.starter.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 }
